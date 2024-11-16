06f7205ad043e5b0a937e0a7c42517b96a62d346
sabriyasar <info@sabriyasar.com.tr>
2024-11-16 17:24:12 +0300

    updated project

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Input, Button, Card, Typography } from "antd";
+import { Input, Button, Card, Typography, Pagination } from "antd";
 import { FileExcelOutlined } from "@ant-design/icons"; // Excel ikonu
 import * as XLSX from "xlsx"; // Excel kütüphanesi
 import "./css/ProductList.css";
@@ -8,10 +8,19 @@ const { Title, Paragraph } = Typography;
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [newProducts, setNewProducts] = useState([]); // Yeni ürünler için state
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [copiedProductIds, setCopiedProductIds] = useState([]);
+  const [previousProductIds, setPreviousProductIds] = useState([]); // Önceki ürün ID'leri
+  const [currentPage, setCurrentPage] = useState(1); // Sayfa numarası
+  const [isNewProductFilterActive, setIsNewProductFilterActive] = useState(false); // Yeni ürün filtresi
+  const [isRemovedProductFilterActive, setIsRemovedProductFilterActive] = useState(false); // Silinen ürün filtresi
+  const productsPerPage = 20; // Her sayfada gösterilecek ürün sayısı
+  const [addedProductCount, setAddedProductCount] = useState(0); // Yeni eklenen ürün sayısı
+  const [removedProductCount, setRemovedProductCount] = useState(0); // Silinen ürün sayısı
+  const [removedProducts, setRemovedProducts] = useState([]); // Silinen ürünler
 
   useEffect(() => {
     const savedCopiedProductIds = JSON.parse(
@@ -19,14 +28,20 @@ const ProductList = () => {
     );
     setCopiedProductIds(savedCopiedProductIds);
 
-    fetch("/products.html")
+    const savedNewProducts = JSON.parse(localStorage.getItem("newProducts") || "[]");
+    const savedPreviousProductIds = JSON.parse(
+      localStorage.getItem("previousProductIds") || "[]"
+    );
+    setPreviousProductIds(savedPreviousProductIds);
+
+    fetch("/products804.html") // Eski ürün sayfası
       .then((response) => response.text())
       .then((html) => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, "text/html");
 
         const productElements = doc.querySelectorAll("div.p-card-wrppr");
-        const productsArray = Array.from(productElements).map((element) => {
+        const previousProductsArray = Array.from(productElements).map((element) => {
           const discountedPriceElement =
             element.querySelector(".prc-box-dscntd");
           const discountedPrice = discountedPriceElement
@@ -54,15 +69,73 @@ const ProductList = () => {
           };
         });
 
-        setProducts(productsArray);
-        setFilteredProducts(productsArray);
-        setLoading(false);
+        // Yeni ürün sayfasını al
+        fetch("/products.html") // Yeni ürün sayfası
+          .then((response) => response.text())
+          .then((html) => {
+            const doc = new DOMParser().parseFromString(html, "text/html");
+            const newProductElements = doc.querySelectorAll("div.p-card-wrppr");
+            const newProductsArray = Array.from(newProductElements).map((element) => {
+              const discountedPriceElement =
+                element.querySelector(".prc-box-dscntd");
+              const discountedPrice = discountedPriceElement
+                ? parseFloat(
+                    discountedPriceElement.innerText
+                      .trim()
+                      .replace("₺", "")
+                      .replace(".", "")
+                      .replace(",", ".")
+                  )
+                : null;
+
+              const linkElement = element.querySelector("a");
+              const productLink = linkElement ? linkElement.href : "#";
+
+              return {
+                id: element.getAttribute("data-id"),
+                name: element.getAttribute("title"),
+                price: {
+                  discountedPrice,
+                  originalPrice: null,
+                  sellingPrice: null,
+                },
+                link: productLink,
+              };
+            });
+
+            // Yeni eklenen ürünleri bul
+            const newAddedProducts = newProductsArray.filter(
+              (product) => !previousProductsArray.some((prev) => prev.id === product.id)
+            );
+
+            // Silinen ürünleri bul
+            const removedProducts = previousProductsArray.filter(
+              (product) => !newProductsArray.some((newProduct) => newProduct.id === product.id)
+            );
+
+            // Yeni eklenen ve silinen ürün sayısını ayarla
+            setAddedProductCount(newAddedProducts.length);
+            setRemovedProductCount(removedProducts.length);
+            setRemovedProducts(removedProducts); // Silinen ürünleri kaydet
+
+            // Yeni eklenen ürünleri kaydet
+            setNewProducts(newAddedProducts); // Yeni ürünler listesine kaydet
+            setPreviousProductIds((prevIds) => [
+              ...prevIds,
+              ...newAddedProducts.map((product) => product.id),
+            ]);
+
+            // Eski ürünlerle karşılaştırıp kaydet
+            setProducts(newProductsArray);
+            setFilteredProducts(newProductsArray);
+            setLoading(false);
+          });
       })
       .catch((err) => {
         console.error("Error fetching products:", err);
         setLoading(false);
       });
-  }, []);
+  }, [previousProductIds]);
 
   const handleSearch = (event) => {
     const searchValue = event.target.value;
@@ -131,6 +204,39 @@ const ProductList = () => {
     XLSX.writeFile(workbook, "urunler.xlsx");
   };
 
+  // Pagination işlemi
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Sayfaya göre ürünleri filtrele
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = isRemovedProductFilterActive
+    ? removedProducts.slice(indexOfFirstProduct, indexOfLastProduct) // Silinen ürünler
+    : isNewProductFilterActive
+    ? newProducts.slice(indexOfFirstProduct, indexOfLastProduct) // Yeni ürünler
+    : filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct); // Tüm ürünler
+
+  // Yeni ürün filtresi butonunun işlevi
+  const toggleNewProductFilter = () => {
+    setIsNewProductFilterActive((prev) => !prev);
+    setCurrentPage(1); // Filtremiz değiştiğinde sayfa numarasını 1'e resetleyelim
+  };
+
+  // Silinen ürün filtresi butonunun işlevi
+  const toggleRemovedProductFilter = () => {
+    setIsRemovedProductFilterActive((prev) => !prev);
+    setCurrentPage(1); // Filtremiz değiştiğinde sayfa numarasını 1'e resetleyelim
+  };
+
+  // Yeni ürün filtresi aktifse pagination'ı ona göre güncelle
+  const totalProducts = isRemovedProductFilterActive
+    ? removedProducts.length
+    : isNewProductFilterActive
+    ? newProducts.length
+    : filteredProducts.length;
+
   return (
     <div className="product-list-container">
       <Title level={1}>Ürün Listesi</Title>
@@ -139,12 +245,40 @@ const ProductList = () => {
         <Paragraph>Yükleniyor...</Paragraph>
       ) : (
         <>
-          <Paragraph>
-            <b style={{ color: "red" }}>
-              Toplam Ürün Sayısı: {filteredProducts.length}
-            </b>
+          <Paragraph
+            style={{
+              backgroundColor: "#001f3d",
+              color: "white",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            <b>Toplam Ürün Sayısı: {filteredProducts.length}</b>
           </Paragraph>
 
+          <Paragraph
+            style={{
+              backgroundColor: "#52c41a",
+              color: "white",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            <b>Yeni Eklenen Ürün Sayısı: {addedProductCount}</b>
+          </Paragraph>
+
+          <Paragraph
+            style={{
+              backgroundColor: "#ff4d4f",
+              color: "white",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            <b>Silinen Ürün Sayısı: {removedProductCount}</b>
+          </Paragraph>
+
+          {/* "Ürün Ara" ve "Temizle" butonları Silinen Ürün Sayısı'nın altına taşındı */}
           <div style={{ marginBottom: 16 }}>
             <Input
               placeholder="Ürün ara..."
@@ -156,19 +290,46 @@ const ProductList = () => {
           </div>
 
           <div style={{ marginBottom: 16 }}>
-            <Button onClick={sortByPriceHighToLow} style={{ marginRight: 8 }}>
-              Fiyatı Yüksekten Düşüğe Sırala
+            <Button
+              onClick={toggleNewProductFilter}
+              style={{
+                marginLeft: 16,
+                backgroundColor: isNewProductFilterActive ? "green" : "#3cb371",
+                borderColor: isNewProductFilterActive ? "green" : "#3cb371",
+                color: "white",
+              }}
+            >
+              {isNewProductFilterActive ? "Filtreyi Kaldır" : "Yeni Ürünleri Göster"}
             </Button>
-            <Button onClick={sortByPriceLowToHigh}>
+            <Button
+              onClick={toggleRemovedProductFilter}
+              style={{
+                marginLeft: 8,
+                backgroundColor: isRemovedProductFilterActive ? "blue" : "#0000FF",
+                borderColor: isRemovedProductFilterActive ? "blue" : "#0000FF",
+                color: "white",
+              }}
+            >
+              {isRemovedProductFilterActive ? "Filtreyi Kaldır" : "Silinen Ürünleri Göster"}
+            </Button>
+            <Button
+              onClick={sortByPriceLowToHigh}
+              style={{ marginLeft: 8 }}
+            >
               Fiyatı Düşükten Yükseğe Sırala
             </Button>
             <Button
+              onClick={sortByPriceHighToLow}
+              style={{ marginLeft: 8 }}
+            >
+              Fiyatı Yüksekten Düşüğe Sırala
+            </Button>
+            <Button
               onClick={downloadExcel}
-              type="primary"
-              icon={<FileExcelOutlined />} // Excel ikonu
+              icon={<FileExcelOutlined />}
               style={{
-                backgroundColor: "#52c41a", // Excel temasına uygun renk
-                borderColor: "#52c41a",
+                backgroundColor: "#007a33",
+                borderColor: "#007a33",
                 color: "white",
                 marginLeft: 8,
               }}
@@ -178,51 +339,60 @@ const ProductList = () => {
           </div>
 
           <div className="product-grid">
-            {filteredProducts.length > 0
-              ? filteredProducts.map((product) => (
-                  <div className="product-card" key={product.id}>
-                    <Card hoverable>
-                      <Card.Meta title={product.name} />
-                      <Paragraph>
-                        Fiyat:{" "}
-                        {product.price.discountedPrice
-                          ? `₺${product.price.discountedPrice.toFixed(2)}`
-                          : "Fiyat Bilgisi Yok"}
-                      </Paragraph>
-                      <Paragraph>
-                        <a
-                          href={product.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Ürün Linki
-                        </a>
-                      </Paragraph>
-                      <Button
-                        onClick={() => handleCopyLink(product.id, product.link)}
-                        style={{
-                          backgroundColor: copiedProductIds.includes(product.id)
-                            ? "green"
-                            : "gray",
-                          color: "white",
-                          marginRight: 8,
-                        }}
-                      >
-                        {copiedProductIds.includes(product.id)
-                          ? "Kopyalandı!"
-                          : "Linki Kopyala"}
-                      </Button>
-                      <Button
-                        onClick={() => handleClearSelection(product.id)}
-                        style={{ backgroundColor: "red", color: "white" }}
-                      >
-                        Seçimi Temizle
-                      </Button>
-                    </Card>
-                  </div>
-                ))
-              : searchTerm !== "" && <Paragraph>Ürün bulunamadı.</Paragraph>}
+            {currentProducts.map((product) => (
+              <div className="product-card" key={product.id}>
+                <Card hoverable>
+                  {newProducts.some((newProduct) => newProduct.id === product.id) && (
+                    <div className="new-product-tag">Yeni Ürün!</div>
+                  )}
+                  <Card.Meta title={product.name} />
+                  <Paragraph>
+                    Fiyat:{" "}
+                    {product.price.discountedPrice
+                      ? `₺${product.price.discountedPrice.toFixed(2)}`
+                      : "Fiyat Bilgisi Yok"}
+                  </Paragraph>
+                  <Paragraph>
+                    <a
+                      href={product.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Ürün Linki
+                    </a>
+                  </Paragraph>
+                  <Button
+                    onClick={() => handleCopyLink(product.id, product.link)}
+                    style={{
+                      backgroundColor: copiedProductIds.includes(product.id)
+                        ? "green"
+                        : "gray",
+                      color: "white",
+                      marginRight: 8,
+                    }}
+                  >
+                    {copiedProductIds.includes(product.id)
+                      ? "Kopyalandı!"
+                      : "Linki Kopyala"}
+                  </Button>
+                  <Button
+                    onClick={() => handleClearSelection(product.id)}
+                    style={{ backgroundColor: "red", color: "white" }}
+                  >
+                    Seçimi Temizle
+                  </Button>
+                </Card>
+              </div>
+            ))}
           </div>
+
+          <Pagination
+            current={currentPage}
+            pageSize={productsPerPage}
+            total={totalProducts}
+            onChange={handlePageChange}
+            style={{ marginTop: 20, textAlign: "center" }}
+          />
         </>
       )}
     </div>
